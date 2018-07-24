d5ec0b3ac9ecc257b288e33731e34731527a3314
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-07-23 20:39:54 -0500

    Proper AI routing CPP

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,19 +2,24 @@
 
 #include "Public/ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "Public/PatrollingGuard.h"// todo temp
+#include "Public/PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	//TODO protect against empty patrol routes
-
 	//Get patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn =AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	// warn about empty patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("a guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
 
 	//set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
