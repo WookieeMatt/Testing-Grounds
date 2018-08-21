5c796b564e20309b38ca7691d06f13abe079e9c0
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-08-21 00:17:22 -0500

    309 complete, Tile CPP spawns random points for actor

---
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,31 @@
+// Curiouser&Curiouser Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	UFUNCTION(BlueprintCallable, Category = "Level Actors")
+	void PlaceActors();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
